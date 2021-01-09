8ccf2411f331eb8b61a41244a755a201ddd3738e
Devon Wolfkiel <devonwolf@tutanota.com>
2021-01-08 18:08:41 -0800

    Define all constants from HTML

---
+++ constants.js
@@ -0,0 +1,33 @@
+const zooParagraph = document.getElementById('zoo');
+const inputDisplay = document.getElementById('inputs')
+
+
+const aOrAn = document.getElementById('a-or-an');
+
+const adj1 = document.getElementById('adj-1');
+const noun1 = document.getElementById('noun-1');
+const verbPast1 = document.getElementById('verb-pt-1');
+const adverb1 = document.getElementById('adverb-1');
+const adj2 = document.getElementById('adj-2');
+const noun2 = document.getElementById('noun2');
+const noun3 = document.getElementById('noun-3');
+const adj3 = document.getElementById('adj-3');
+const verb1 = document.getElementById('verb-1');
+const adverb2 = document.getElementById('adverb-2');
+const verbPast2 = document.getElementById('verb-pt-2');
+const adj4 = document.getElementById('adj-4');
+
+const adj1 = document.getElementById('adj-1-input');
+const noun1 = document.getElementById('noun-1-input');
+const verbPast1 = document.getElementById('verb-pt-1-input');
+const adverb1 = document.getElementById('adverb-1-input');
+const adj2 = document.getElementById('adj-2-input');
+const noun2 = document.getElementById('noun-2-input');
+const noun3 = document.getElementById('noun-3-input');
+const adj3 = document.getElementById('adj-3-input');
+const verb1 = document.getElementById('verb-1-input');
+const adverb2 = document.getElementById('adverb-2-input');
+const verbPast2 = document.getElementById('verb-pt-2-input');
+const adj4 = document.getElementById('adj-4-input');
+
+const submitButton = document.getElementById('submitBtn');
